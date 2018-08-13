f76c74ae85523b43aac3452f3b093d8fee24664d
Hector <hector@electricgnome.com>
2018-08-13 14:32:40 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Tabs/>
+  </v-app >
 </template>
-
+  
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Tabs from './components/Tabs'
+
 
 export default {
-  name: "app",
+  name: 'App',
   components: {
-    HelloWorld
+    Tabs
   }
-};
+ 
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  background-image: url("http://www.electricgnome.com/codiad/workspace/Kappa/image/polonez_car.png");
 }
+
 </style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-toolbar
+      color="red accent-2"
+      dark
+      tabs
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title>Kappa Insurance</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Main</v-btn>
+            <v-btn flat>Quote Request</v-btn>
+            <v-btn flat>Add customer</v-btn>
+
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>more_vert</v-icon>
+            </v-btn>
+        </v-toolbar-items>
+      <v-tabs
+        slot="extension"
+        v-model="tab"
+        color="red accent-2"
+        grow
+      >
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab
+          v-for="item in items"
+          :key="item"
+        >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card flat>
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+
+<script>
+export default {
+   name: 'Tabs',
+  data () {
+    return {
+      tab: null,
+      items: [
+        'Drivers', 'Vehicles', 'Discounts'
+      ],
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
