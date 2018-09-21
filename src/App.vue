b73891b9945eec49cf80594fc7c587ace5de360b
Hector <hector@electricgnome.com>
2018-09-21 12:19:23 -0500

    mock app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <v-app>
-    <NavBar/>
-   <!-- <Login/> -->
-     <router-view></router-view>
-  </v-app >
+<v-app>
+    <NavBar />
+    <!-- <Login/> -->
+    <router-view></router-view>
+</v-app>
 </template>
+
   
+
 <script>
 import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 
 export default {
-  name: "App",
-  components: {
-    NavBar,
-    Login
-   
-  }
+    name: "App",
+    components: {
+        NavBar,
+        Login,
+    }
 };
 </script>
